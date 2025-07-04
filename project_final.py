b48fd42a84e96d67ac27f4dd872f9954e0f8f8a4
devilufa <k.ilyas2000@gmail.com>
2025-07-04 21:42:49 +0300

    Update project_final.py

--- project_final.py
+++ project_final.py
@@ -1,3 +1,5 @@
+from dotenv import load_dotenv
+load_dotenv() 
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.responses import StreamingResponse
 from .schemas import ImageRequest
